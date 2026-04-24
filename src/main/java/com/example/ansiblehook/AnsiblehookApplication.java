0b5f7474afee9747548490d379ec6d0071c1babd
wlanboy <wlanboy@github.com>
2026-04-24 16:02:37 +0200

    Added Project files

---
+++ src/main/java/com/example/ansiblehook/AnsiblehookApplication.java
@@ -0,0 +1,13 @@
+package com.example.ansiblehook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsiblehookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsiblehookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ansiblehook/AnsiblehookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ansiblehook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsiblehookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
