89aeae848f73fc5547651275785d4bb1f34b8758
wlanboy <wlanboy@github.com>
2026-04-25 07:08:45 +0200

    EnableScheduling

--- src/main/java/com/example/ansiblehook/AnsiblehookApplication.java
+++ src/main/java/com/example/ansiblehook/AnsiblehookApplication.java
@@ -3,8 +3,10 @@ package com.example.ansiblehook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(AnsiblehookProperties.class)
 public class AnsiblehookApplication {
 
